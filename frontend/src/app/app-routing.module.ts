54c9c89883cd53796f761836c6d997288a0253df
Laura Garcia <garcia.hdez.laura@gmail.com>
2023-02-10 17:55:44 +0100

    components created + bootstrap configured

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { RecoveryComponent } from './auth/recovery/recovery.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { Error404Component } from './components/errors/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'recovery', component: RecoveryComponent},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'gallery/:id', component: GalleryComponent},
+  { path: 'editor', component: EditorComponent},
+  { path: '404', component: Error404Component},
+  { path: '', redirectTo: '/landing', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,11 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';import { GuardsComponent } from './guards/guards.component';
+import { InterfacesComponent } from './interfaces/interfaces.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { RecoveryComponent } from './auth/recovery/recovery.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { EditorComponent } from './pages/editor/editor.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { Error404Component } from './components/errors/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuardsComponent,
+    InterfacesComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    NavBarComponent,
+    FooterComponent,
+    GalleryComponent,
+    EditorComponent,
+    ProfileComponent,
+    ChangePasswordComponent,
+    LandingPageComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
