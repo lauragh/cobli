fa26f8c6f0b2c4b7dd51aea3deb7e5200d8ce1d7
Laura Garcia <garcia.hdez.laura@gmail.com>
2023-02-18 15:27:36 +0100

    landing page slider1 + 2 done

--- frontend/src/app/pages/gallery/gallery.component.ts
+++ frontend/src/app/pages/gallery/gallery.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit{
+  animation: any;
+  botonMostrar: any;
+  threshold = 100;
+
+  @ViewChild('elemento') elemento!: ElementRef;
+
+  constructor(
+    private renderer2: Renderer2,
+   ) {}
+
+  ngOnInit(): void {
+    this.animation = document.getElementById('my-element');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > this.threshold) {
+        this.animation.classList.add('fade-out');
+      } else {
+        this.animation.classList.remove('fade-out');
+      }
+    });
+  }
+
+  ocultar(): void {
+    this.renderer2.setAttribute(this.elemento.nativeElement,'class','oculto');
+  }
+
+  mostrar(): void {
+    this.renderer2.removeAttribute(this.elemento.nativeElement,'class','oculto');
+
+  }
 
 }
--- frontend/src/app/pages/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+  @ViewChild('slider1') slider1!: ElementRef;
+  @ViewChild('slider2') slider2!: ElementRef;
 
+  sliders: String[] = ["slider1","slider2", "slider 3", "slider 4"];
+
+  constructor(
+    private renderer2: Renderer2,
+   ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  ocultar(): void {
+    this.renderer2.addClass(this.slider1.nativeElement,'oculto');
+    this.renderer2.removeClass(this.slider2.nativeElement,'display-none');
+    setTimeout(() => {
+      this.renderer2.addClass(this.slider2.nativeElement,'oculto2');
+    }, 500);
+
+
+
+    // setTimeout(() => {
+    //   this.renderer2.removeClass(this.slider1.nativeElement,'oculto')
+    // }, 2000);
+    // this.renderer2.setAttribute(this.slider1.nativeElement,'class','oculto');
+
+  }
+
+  mostrar(): void {
+    console.log('hola');
+    this.renderer2.removeClass(this.slider1.nativeElement,'oculto');
+    // this.renderer2.addClass(this.slider1.nativeElement,'mostrar')
+
+    // this.renderer2.removeAttribute(this.slider1.nativeElement,'class','oculto');
+  }
 }
